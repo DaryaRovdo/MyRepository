f189c7b858477394feccc3cb969eaa47ec28e291
Darya Rovdo <d.rovdo@gmail.com>
2015-02-17 17:33:03 +0300

    Juices adding

---
+++ Task1/src/task1/Juice.java
@@ -0,0 +1,13 @@
+package task1;
+
+import java.util.*;
+
+public class Juice {
+        public TreeSet <String> components;
+        Juice() {
+            components = new TreeSet ();
+        }
+        public void addComponent(String s) {
+            components.add(s);
+        }
+}
---
+++ Task1/src/task1/SortThread.java
@@ -0,0 +1,19 @@
+package task1;
+
+import java.util.*;
+
+public class SortThread extends Thread{
+        ArrayList al;
+        SortThread (ArrayList al) {
+            this.al = al;
+        }
+        @Override
+        public void run() {
+            Collections.sort(al, new Comparator <String> () {
+                @Override
+                public int compare(String s1, String s2) {
+                    return s1.compareTo(s2);
+                }
+            });
+        }
+}
