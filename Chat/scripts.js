e0f103d6484bb14ceb0f5ade6f45bee4a6396e63
Darya Rovdo <d.rovdo@gmail.com>
2015-03-09 20:23:40 +0300

    delete and edit

--- Chat/scripts.js
+++ Chat/scripts.js
@@ -4,59 +4,77 @@ function run(){
 }
 
 function delegateEvent(evtObj) {
-	if(evtObj.type === "click" && evtObj.target.id.contains("send")) {
+	if(evtObj.type === "click" && evtObj.target.id.contains("sendBtn")) {
 		onSendButtonClick(evtObj);
 	}
-	if(evtObj.type == "click" && evtObj.target.id.contains("change")) {
-		onChangeButtonClick(evtObj);
+	if(evtObj.type == "click" && evtObj.target.id.contains("changeNameBtn")) {
+		onChangeNameButtonClick(evtObj);
 	}
 	if(evtObj.type == "click" && evtObj.target.id.contains("iconDelete")) {
-		onDeleteClick(evtObj);
+		onIconDeleteClick(evtObj);
 	}
 	if(evtObj.type == "click" && evtObj.target.id.contains("iconEdit")) {
-		onEditClick(evtObj);
+		onIconEditClick(evtObj);
 	}
 }
-function onDeleteClick() {
-	alert("it works!");
+function onIconDeleteClick(evtObj) {
+	var conf = confirm("Confirm removal, please :)");
+	if (conf == true)
+	{
+		evtObj.target.parentNode.parentNode.removeChild(evtObj.target.parentNode);
+	}
 }
-function onEditClick() {
-	alert("it works!");
+var messageToEdit;
+function onIconEditClick(evtObj) {
+	var message = evtObj.target.parentNode;
+	var messageArea = document.getElementById("messageText");
+	messageArea.value = message.getElementsByTagName("div")[0].getElementsByTagName("span")[1].innerHTML;
+	var sendButton = document.getElementById("sendBtn");
+	sendBtn.innerHTML = "Edit";
+	messageToEdit = message;
 }
 function onSendButtonClick(){
-	if (document.getElementById("change").innerHTML == "Enter name" || document.getElementById("name").value == "")
+	if (document.getElementById("changeNameBtn").innerHTML == "Enter name" || document.getElementById("name").value == "")
 		alert("Enter your name, please :)");
-	else if (document.getElementById("text").value == "")
+	else if (document.getElementById("messageText").value == "")
 		alert("Your message is empty! :(");
-	else {
-		var text = document.getElementById("text");
+	else if (document.getElementById("sendBtn").innerHTML != "Edit") {
+		var text = document.getElementById("messageText");
 		addMessage(text.value);
 		text.value = "";
 	}
+	else {
+		document.getElementById("sendBtn").innerHTML = "Send";
+		var text = document.getElementById("messageText").value;
+		document.getElementById("messageText").value = "";
+		messageToEdit.getElementsByTagName("div")[0].getElementsByTagName("span")[1].innerHTML = text;
+	}
 }
-function onChangeButtonClick() {
+function onChangeNameButtonClick() {
 	var nameText = document.getElementById("enterName");
-	document.getElementById("change").innerHTML = "Change name";
+	document.getElementById("changeNameBtn").innerHTML = "Change name";
 	document.getElementById("name").innerHTML = nameText.value;
 	nameText.value = "";
 }
 function addMessage(text){
 	var divItem = document.createElement("div");
 	var divText = document.createElement("div");
-	var spanItem = document.createElement("span");
+	var spanName = document.createElement("span");
+	var spanText = document.createElement("span");
 	var deleteImg = document.createElement("img");
 	var editImg = document.createElement("img");
 	divItem.classList.add("userMessage");
-	spanItem.classList.add("userName");
+	spanName.classList.add("userName");
 	deleteImg.id =  "iconDelete";
 	deleteImg.setAttribute("src", "delete.png");
 	deleteImg.setAttribute("alt", "Delete message");
 	editImg.id = "iconEdit";
 	editImg.setAttribute("src", "edit.png");
 	editImg.setAttribute("alt", "Edit message");
-	spanItem.innerHTML = document.getElementById("name").value + ":&nbsp";
-	divText.appendChild(spanItem);
-	divText.innerHTML += text;
+	spanName.innerHTML = document.getElementById("name").value + ":&nbsp";
+	spanText.innerHTML = text;
+	divText.appendChild(spanName);
+	divText.appendChild(spanText);
 	divItem.appendChild(divText);
 	divItem.appendChild(deleteImg);
 	divItem.appendChild(editImg);
